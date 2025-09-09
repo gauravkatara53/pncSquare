44cbf4601ba58de355f8f9d0d30dd28139d64994
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-10 02:03:10 +0530

    add

--- src/app/college/[slug]/page.tsx
+++ src/app/college/[slug]/page.tsx
@@ -1,4 +1,3 @@
-// app/college/[slug]/page.tsx
 import { Metadata } from "next";
 import { apiService } from "@/ApiService/apiService";
 import CollegePageClient from "@/components/PlacementPage/CollegePageClient";
@@ -20,28 +19,28 @@ interface CollegeResponse {
   success: boolean;
 }
 
-interface PageProps {
-  params: Promise<{ slug: string }>; // 👈 async params
-}
+type Params = Promise<{ slug: string }>;
 
 export async function generateMetadata({
   params,
-}: PageProps): Promise<Metadata> {
-  const { slug } = await params; // 👈 await here
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { slug } = await params;
 
   try {
     const response = await apiService.get<CollegeResponse>(`/college/${slug}`);
     const college = response?.data;
 
-    // if (!college) {
-    //   return {
-    //     title: "College Not Found | Placements & Cutoffs",
-    //     description: "Requested college data not found.",
-    //   };
-    // }
+    if (!college) {
+      return {
+        title: "College Not Found | Placements & Cutoffs",
+        description: "Requested college data not found.",
+      };
+    }
 
     return {
-      title: `${college.name} –  Admission, Cutoff, Fees, Courses & Placements 2025 | PNC Square`,
+      title: `${college.name} – Admission, Cutoff, Fees, Courses & Placements 2025 | PNC Square`,
       description:
         college.bio ||
         `Explore ${college.name} - Admission, Cutoff, Fees, Courses & Placements 2025 | PNC Square`,
@@ -68,7 +67,7 @@ export async function generateMetadata({
   }
 }
 
-export default async function CollegePage({ params }: PageProps) {
-  const { slug } = await params; // 👈 await here
-  return <CollegePageClient slug={slug} />; // 👈 pass plain string
+export default async function CollegePage({ params }: { params: Params }) {
+  const { slug } = await params;
+  return <CollegePageClient slug={slug} />;
 }
