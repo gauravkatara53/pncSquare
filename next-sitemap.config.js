bd41e8d325bf1279286d549dc1ef69787c481aba
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-13 22:03:38 +0530

    adD

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteUrl: "https://pncsquare.in",
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  exclude: ["/server-sitemap.xml"],
+  exclude: ["/_not-found"], // don’t include error routes
+  generateIndexSitemap: false, // force single sitemap with urls instead of nested indexes
 };
