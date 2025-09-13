2fa6db1d0a2d24ce6df758304c6734b8db452876
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-13 21:56:25 +0530

    add

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://pncsquare.in", // replace with your domain
+  siteUrl: "https://pncsquare.in",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   exclude: ["/server-sitemap.xml"],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://pncsquare.in/sitemap.xml"],
-  },
 };
