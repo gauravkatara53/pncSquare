2481638cc3f9ccc00da8719800b2f142af28f96e
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-29 13:23:09 +0530

    add

--- src/app/signin/[[...rest]]/page.tsx
+++ src/app/signin/[[...rest]]/page.tsx
@@ -3,12 +3,8 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  console.log("Rendering Clerk SignIn page...");
-
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <h1>Main heading</h1>
-      <p>This is the para section</p>
       <div className="p-8 bg-white rounded shadow-md w-[400px]">
         <SignIn path="/signin" routing="path" signUpUrl="/signup" />
       </div>
