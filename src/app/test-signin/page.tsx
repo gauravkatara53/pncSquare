96e452f3dee9503597804a3fbebadecc249aec34
GAURAV KATARA <gauravkatara53@gmail.com>
2025-09-29 13:03:58 +0530

    clerk

--- src/app/test-signin/page.tsx
+++ src/app/test-signin/page.tsx
@@ -5,8 +5,10 @@ import { SignIn } from "@clerk/nextjs";
 export default function TestSignInPage() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <h1>main heading </h1>
+      <p>this is the para section </p>
       <div className="p-8 bg-white rounded shadow-md w-[400px]">
-        <SignIn routing="hash" />
+        <SignIn />
       </div>
     </div>
   );
