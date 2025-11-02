6d0b4ce94812338ecaa15c8ccb650adcf837270f
GAURAV KATARA <gauravkatara53@gmail.com>
2025-11-02 14:24:26 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://pnc-backend.onrender.com/api/:path*",
+        destination: "http://13.203.80.218/api/:path*",
       },
     ];
   },
